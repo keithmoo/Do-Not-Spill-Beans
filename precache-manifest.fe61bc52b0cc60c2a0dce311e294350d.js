baa6684c2d5e150c8fd22d396b4295f37b137ffa
Keith Freitag <keithfreitag@gmail.com>
2020-05-23 13:04:18 -0700

    Updates

---
+++ precache-manifest.fe61bc52b0cc60c2a0dce311e294350d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e7522ff7f5c13fe871e73b2e832563b",
+    "url": "/Do-Not-Spill-Beans/index.html"
+  },
+  {
+    "revision": "a75ebc8d5a421cf19a57",
+    "url": "/Do-Not-Spill-Beans/static/css/main.e2f38fd9.chunk.css"
+  },
+  {
+    "revision": "83f2f5ef3da8255ab196",
+    "url": "/Do-Not-Spill-Beans/static/js/2.64dfb9cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Do-Not-Spill-Beans/static/js/2.64dfb9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a75ebc8d5a421cf19a57",
+    "url": "/Do-Not-Spill-Beans/static/js/main.cd6c5c13.chunk.js"
+  },
+  {
+    "revision": "760c8a1c7d9400e6ce1e",
+    "url": "/Do-Not-Spill-Beans/static/js/runtime-main.1ba5d165.js"
+  },
+  {
+    "revision": "e62fdc1a94859f906ae578e0e8479f78",
+    "url": "/Do-Not-Spill-Beans/static/media/bean.e62fdc1a.png"
+  },
+  {
+    "revision": "82ed41b4877d438138865561db4a657c",
+    "url": "/Do-Not-Spill-Beans/static/media/killerBean.82ed41b4.png"
+  }
+]);
